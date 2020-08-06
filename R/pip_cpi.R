e967bc9d176d728b0fa2ae323f1fd8aaa063907f
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-08-06 11:58:12 -0400

    create pip_cpi_update

--- R/pip_cpi.R
+++ R/pip_cpi.R
@@ -69,17 +69,44 @@ pip_cpi_update <- function(cpidir,
   cpidlw <- paste0(dlwdir, "Support_2005_CPI_v04_M/Data/Stata/Final_CPI_PPP_to_be_used.dta")
 
   cpi    <- haven::read_dta(cpidlw)
+
+  # Note clean CPI data file and then create datasignature
   ds_dlw <- digest::digest(cpi,  algo = "xxhash64") # Data signature of file
 
   # check signature of current fst file
+  ds_production_path <- paste0(cpidir, "cpi_datasignature.txt")  # data signature in production
 
 
+  if (file.exists(ds_production_path)) {
+    # read data signature in production
+    ds_production <- readr::read_lines(ds_production_path)
 
+  } else {
+    ds_production <- "0000" # fake signature
+  }
 
+  #--------- if Signature from dlw is differnet from the one in production ---------
 
+  if (ds_dlw != ds_production) {
 
+    # re-write cpi in production if data signature is not found
+    fst::write_fst(x = cpi,
+                   path = paste0(cpidir, "cpi.fst")
+    )
+    # Vintage
+    time <- format(Sys.time(), "%Y%m%d%H%M%S") # find a way to account for time zones
+    fst::write_fst(x = cpi,
+                   path = paste0(cpidir, "_vintage/cpi_", time,".fst")
+    )
 
-
+    readr::write_lines(x = ds_dlw,
+                       path = ds_production_path)
+    print("Data signature was not found, so cpi.fst has been updated")
+    return(invisible(TRUE))
+  } else {
+    print("Data signature is up to date. No update performed")
+    return(invisible(FALSE))
+  }
 }
 
 #--------- Find latest dlw ---------
