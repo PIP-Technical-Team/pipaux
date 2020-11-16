50499ed94830f338df204c2f3e95f48177d51282
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-11-16 18:46:01 -0500

    Include vintage level in CPI

--- R/pip_cpi_update.R
+++ R/pip_cpi_update.R
@@ -7,19 +7,42 @@
 #' @export
 #'
 #' @examples
-pip_cpi_update <- function(msrdir, dlwdir, force){
+pip_cpi_update <- function(msrdir = paste0(getOption("pipaux.maindir"), "_aux/cpi/"),
+                           dlwdir = getOption("pipaux.dlwdir"),
+                           force  = FALSE){
 
   # check for last version in dlw
-  dlwdir_l   <- latest_dlw_dir(dlwdir = dlwdir) # from utils.R
-  cpidlw_dir <- paste0(dlwdir, dlwdir_l, "/", dlwdir_l, "_CPIICP.dta")
+  change_vintage1   <- vintage_level_1("cpi",
+                                msrdir = msrdir,
+                                dlwdir = dlwdir,
+                                force  =  force)
 
-  cpidlw     <- haven::read_dta(cpidlw_dir)
-  cpi        <- pip_cpi_clean(cpidlw)
+  if (change_vintage1 == TRUE || force == TRUE) {
 
-  pip_sign_save(x       = cpi,
-                measure = "cpi",
-                msrdir  = msrdir,
-                force   = force)
+    change_vintage2   <- vintage_level_2("cpi",
+                                  msrdir = msrdir,
+                                  dlwdir = dlwdir,
+                                  force  =  force)
+  } else {
+    change_vintage2 <- FALSE
+  }
+
+
+  if (change_vintage2 == TRUE || force == TRUE) {
+
+    dlwdir_l   <- latest_dlw_dir(dlwdir = dlwdir) # from utils.R
+    cpidlw_dir <- paste0(dlwdir, dlwdir_l, "/", dlwdir_l, "_CPIICP.dta")
+
+    cpidlw     <- haven::read_dta(cpidlw_dir)
+    cpi        <- pip_cpi_clean(cpidlw, cpi_id = dlwdir_l)
+
+    pip_sign_save(x       = cpi,
+                  measure = "cpi",
+                  msrdir  = msrdir,
+                  force   = force)
+  } else {
+    cli::cli_alert_success("cpi data is up to date")
+  }
 
 }
 
