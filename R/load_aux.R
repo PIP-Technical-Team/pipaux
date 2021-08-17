d5307f44fa98cf88a5fb7a236fabc1fa738e85f9
Aleksander Eilertsen <aeilertsen@worldbank.org>
2021-08-17 07:31:45 -0400

    Fix bug
    Fix bug for loading cp data

--- R/load_aux.R
+++ R/load_aux.R
@@ -19,12 +19,17 @@ load_aux <- function(measure = NULL,
 
   }
   # check file exists
-  if (file.exists(paste0(msrdir, measure ,".fst"))) {
+  if (file.exists(paste0(msrdir, measure , ".fst"))) {
 
-    df <- fst::read_fst(paste0(msrdir, measure ,".fst"))
+    df <- fst::read_fst(paste0(msrdir, measure ,".fst"), as.data.table = TRUE)
+
+  } else if (file.exists(paste0(msrdir, measure ,".rds"))) {
+
+    df <- readRDS(paste0(msrdir, measure, '.rds'))
 
   } else {
-    msg <- paste("file `", measure,".fst` does not exist.")
+
+    msg <- sprintf("file for measure `%s` does not exist.",  measure)
     rlang::abort(c(
       msg,
       i = "check your connection or data availability"
