fa9b3cb259d5f1b5ea2ae824528844848e530ecf
Aleksander Eilertsen <aeilertsen@worldbank.org>
2021-10-13 14:37:52 -0400

    Update metadata
    Recreate survey metadata as a nested data frame with both info and
    metadata.

--- R/pip_metadata.R
+++ R/pip_metadata.R
@@ -6,14 +6,14 @@
 #' @export
 pip_metadata <- function(action = "update",
                          force = FALSE,
-                         maindir = gls$PIP_DATA_DIR) {
+                         maindir = getOption("pipaux.maindir")) {
   measure <- "metadata"
   msrdir <- paste0(maindir, "_aux/", measure, "/")
 
   if (action == "update") {
 
     # Load pfw
-    pfw <- load_aux("pfw")
+    pfw <- load_aux("pfw", maindir = maindir)
     pfw <- data.table::setDT(pfw)
     pfw$link <- with(
       pfw,
@@ -33,44 +33,95 @@ pip_metadata <- function(action = "update",
     path <- sort(files, decreasing = TRUE)[1]
     df <- readxl::read_xlsx(path)
 
+    # Create distribution type column (data type)
+    domain_check <- with(pfw, (gdp_domain == 2 | pce_domain == 2 |
+                                 pop_domain == 2 | cpi_domain == 2 |
+                                 ppp_domain == 2))
+    pfw$distribution_type <- ifelse(pfw$use_imputed == 1,
+                                    "imputed", NA_character_)
+    pfw$distribution_type <- ifelse(pfw$use_microdata == 1,
+                                    "micro", pfw$distribution_type)
+    pfw$distribution_type <- ifelse(pfw$use_groupdata == 1,
+                                    "group",
+                                    pfw$distribution_type)
+    pfw$distribution_type <- ifelse(pfw$use_groupdata == 1 & domain_check,
+                                    "aggregated",
+                                    pfw$distribution_type)
     # Merge datasets (inner join)
     df <-
       merge(df,
-        pfw[, c("country_code", "surveyid_year", "survey_acronym", "link")],
+        pfw[, c("country_code", "surveyid_year", "survey_acronym",
+                "welfare_type", "reporting_year", "distribution_type",
+                "surv_producer",
+                "link")],
         by = "link", all = FALSE
       )
 
+
     # Recode colnames
     df <- df %>%
-      data.table::setnames("title", "survey_title")
+      data.table::setnames(c("title", "surv_producer", "coverage"),
+                           c("survey_title", "survey_conductor", "survey_coverage"))
 
     # Select columns
     df <- df[
       c(
-        "country_code", "surveyid_year", "survey_acronym",
+        "country_code", "reporting_year",
+        "surveyid_year", "survey_acronym",
+        "survey_conductor", "survey_coverage",
+        "welfare_type", "distribution_type",
         "survey_title", "year_start", "year_end",
         "authoring_entity_name", "abstract",
         "collection_dates_cycle", "collection_dates_start",
-        "collection_dates_end", "coverage",
+        "collection_dates_end",
         "sampling_procedure", "collection_mode",
         "coll_situation", "weight", "cleaning_operations"
       )
     ]
     df <- data.table::setDT(df)
 
-    # Sign and save
-    pip_sign_save(
-      x = df,
-      measure = "metadata",
-      msrdir = msrdir,
-      force = force,
-      save_dta = FALSE
+    # Create nested table
+    df <- tidyfast::dt_nest(df, country_code, reporting_year, survey_title,
+                            survey_conductor, survey_coverage, welfare_type,
+                            distribution_type, .key = "metadata")
+
+    # Check hash
+    hash <- digest::digest(df, algo = "xxhash64")
+    current_hash <- tryCatch(
+      readr::read_lines(paste0(maindir, "_aux/metadata/metadata_datasignature.txt")),
+      error = function(e) NULL
     )
+
+    # Save data
+    if (hash != current_hash || force || is.null(current_hash)) {
+
+      # Create vintage folder
+      wholedir <- paste0(maindir, "_aux/metadata/_vintage/")
+      if (!(dir.exists(wholedir))) {
+        dir.create(wholedir, recursive = TRUE)
+      }
+
+      # Write files
+      time <- format(Sys.time(), "%Y%m%d%H%M%S")
+      saveRDS(df, paste0(maindir, "_aux/metadata/metadata.rds"))
+      saveRDS(df, sprintf("%s_aux/metadata/_vintage/metadata_%s.rds", maindir, time))
+      readr::write_lines(hash, file = paste0(maindir, "_aux/metadata/metadata_datasignature.txt"))
+
+      # Print msg
+      infmsg <- paste(
+        "Data signature has changed, it was not found,",
+        "or update was forced.\n",
+        paste0("`", "metadata", ".rds` has been updated")
+      )
+      rlang::inform(infmsg)
+      return(invisible(TRUE))
+    } else {
+      rlang::inform("Data signature is up to date.\nNo update performed")
+      return(invisible(FALSE))
+    }
+
   } else if (action == "load") {
-    df <- load_aux(
-      maindir = maindir,
-      measure = measure
-    )
+    df <- readRDS(paste0(maindir, "_aux/metadata/metadata.rds"))
     return(df)
   } else {
     msg <- paste("action `", action, "` is not a valid action.")
