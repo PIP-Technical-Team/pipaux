e8034be75d1420e83e046e5f7e44721bb0e5243f
Aleksander Eilertsen <aeilertsen@worldbank.org>
2022-01-11 11:27:06 -0500

    Add new poverty lines functions

---
+++ R/poverty_lines.R
@@ -0,0 +1,73 @@
+#' Load raw poverty lines data
+#'
+#' @inheritParams load_raw
+#' @return list
+#' @keywords internal
+load_poverty_lines <- function(file, maindir){
+
+  assertthat::assert_that(tools::file_ext(file) == "yaml",
+                          msg = "File extention must be .yaml")
+
+  mrsdir <- "pl"
+  path <- fs::path(maindir, mrsdir, file)
+  yaml::read_yaml(path)
+
+}
+
+#' Transform poverty lines
+#'
+#' @param dl list: Censoring object. Output of `load_poverty_lines()`.
+#' @return list
+#' @keywords internal
+transform_poverty_lines <- function(dl){
+
+  pls <- seq(dl$min, dl$max, dl$increment)
+  df <- data.table::data.table(
+    name = as.character(pls),
+    poverty_line = pls
+  )
+  df$is_default <- ifelse(df$name == dl$default, TRUE, FALSE)
+  df$is_visible <- ifelse(df$name %in% dl$visible, TRUE, FALSE)
+  df$name <- ifelse(nchar(df$name) == 3, sprintf("%s0", df$name), df$name)
+  df$name <- ifelse(nchar(df$name) == 1, sprintf("%s.00", df$name), df$name)
+
+  return(df)
+
+}
+
+#' Verify poverty lines input data
+#'
+#' Verify that the raw poverty lines data has the correct structure.
+#'
+#' @param dl list: Poverty lines object.
+#' @keywords internal
+#' @return list
+verify_input_poverty_lines <- function(dl){
+
+  assertthat::assert_that(all(names(dl) == c("default", "min", "max", "visible", "increment")))
+  return(dl)
+
+}
+
+#' Verify poverty lines output data
+#'
+#' Verify that the transformed poverty lines data has the correct structure.
+#'
+#' @param df data.frame: Poverty lines object.
+#' @keywords internal
+#' @return list
+verify_output_poverty_lines <- function(df){
+
+  df %>%
+    assertr::verify(assertr::has_only_names(
+      "name","poverty_line", "is_default","is_visible")) %>%
+    assertr::verify(assertr::has_class("poverty_line", class = "numeric")) %>%
+    assertr::verify(assertr::has_class("name", class = "character")) %>%
+    assertr::verify(assertr::has_class("is_default", "is_visible", class = "logical")) %>%
+    assertr::verify(nchar(name) == 4) %>%
+    assertr::verify(sum(is_default) == 1)
+
+  return(df)
+
+}
+
