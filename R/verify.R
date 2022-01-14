3523b0da60e9145054a97b64a6d1b8d686ee85b2
Aleksander Eilertsen <aeilertsen@worldbank.org>
2022-01-14 08:34:57 -0500

    Diverse minor changes

--- R/verify.R
+++ R/verify.R
@@ -1,36 +1,41 @@
 #' Verify input
 #'
-#' @param df data.frame: A table with raw data.
-#' @param x character: Type of dataset.
+#' Verify that a raw data object (directly from source) has the correct structure.
+#'
+#' @param x Data object to validate.
+#' @param measure character: Measure that the data object belongs to.
 #' @export
-#' @return data.frame
-verify_input <- function(df, x){
-  select_verify_input[[x]](df)
+verify_input <- function(x, measure){
+  select_verify_input[[measure]](x)
 }
 
 #' Select verify input
 #' @inheritParams verify_input
 #' @noRd
-#' @return data.frame
 select_verify_input <- list(
   maddison = function(...) verify_input_maddison(...),
-  censoring = function(...) verify_input_censoring(...)
+  censoring = function(...) verify_input_censoring(...),
+  cpi = function(...) verify_input_cpi(...),
+  country_profiles = function(...) verify_input_country_profiles(...)
+
 )
 
 #' Verify output
 #'
+#' Verify that a transformed data object has the correct structure.
+#'
 #' @inheritParams verify_input
 #' @export
-#' @return data.frame
-verify_output <- function(df, x){
-  select_output_input[[x]](df)
+verify_output <- function(x, measure){
+  select_output_input[[measure]](x)
 }
 
 #' Select verify input
 #' @inheritParams verify_input
 #' @noRd
-#' @return data.frame
-select_output_input <- list(
+select_verify_input <- list(
   maddison = function(...) verify_output_maddison(...),
-  censoring = function(...) verify_output_censoring(...)
+  censoring = function(...) verify_output_censoring(...),
+  cpi = function(...) verify_ouptut_cpi(...),
+  country_profiles = function(...) verify_output_country_profiles(...)
 )
