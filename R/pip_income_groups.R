b72ca945e52482bc984bff32d75fd525c6f58518
Aleksander Eilertsen <aeilertsen@worldbank.org>
2022-01-26 10:46:54 -0500

    Add function to create income groups dataset
    Add pip_income_groups() to load data from "CLASS.dta" and
    create a corresponding .fst file
    
    See #65

---
+++ R/pip_income_groups.R
@@ -0,0 +1,38 @@
+#' PIP income groups
+#'
+#' Update or load a dataset with historical income groups
+#'
+#' @inheritParams pip_prices
+#' @export
+pip_income_groups <- function(action = "update",
+                              force = FALSE,
+                              maindir = gls$PIP_DATA_DIR) {
+  measure <- "income_groups"
+  msrdir <- paste0(maindir, "_aux/", measure, "/")
+
+  if (action == "update") {
+    df <- haven::read_dta(paste0(msrdir, "CLASS.dta"))
+    df <- df[c('code', 'year_data', 'incgroup_historical')]
+    names(df)[1] <- 'country_code'
+    pip_sign_save(
+      x = df,
+      measure = measure,
+      msrdir = msrdir,
+      force = force
+    )
+  } else if (action == "load") {
+    df <- load_aux(
+      maindir = maindir,
+      measure = measure
+    )
+    return(df)
+  } else {
+    msg <- paste("action `", action, "` is not a valid action.")
+    rlang::abort(c(
+      msg,
+      i = "make sure you select `update` or `load`"
+    ),
+    class = "pipaux_error"
+    )
+  }
+}
