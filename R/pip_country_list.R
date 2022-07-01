63924575c64c65d957dc9cdfa9d817a95eeddca0
R.Andres Castaneda <acastanedaa@worldbank.org>
2022-07-01 12:11:13 -0400

    update pip_country_list() to read from GH raw data

--- R/pip_country_list.R
+++ R/pip_country_list.R
@@ -8,92 +8,48 @@
 #'
 #' The dependency on the PCN Masterfile should be changed in the future.
 #'
-#' @inheritParams pip_prices
-#' @param pcndir character: PovcalNet directory for the Masterfile.
+#' @inheritParams pip_cpi
+#' @inheritParams pip_cpi_update
 #' @export
-pip_country_list <- function(action = "update",
-                             force = FALSE,
-                             pcndir = gls$PCN_MASTER,
-                             maindir = gls$PIP_DATA_DIR) {
+#' @return logical if `action = "update"` or data.table if `action = "load"`
+pip_country_list <- function(action = c("update", "load"),
+                             maindir = gls$PIP_DATA_DIR,
+                             force   = FALSE,
+                             owner   = "PIP-Technical-Team",
+                             repo    = "aux_cpi",
+                             branch  = c("DEV", "PROD", "main"),
+                             tag     = match.arg(branch)) {
   measure <- "country_list"
-  msrdir <- fs::path(maindir, "_aux/", measure) # measure dir
+  branch  <- match.arg(branch)
+  action  <- match.arg(action)
 
   if (action == "update") {
 
-    # ---- Read PCN Masterfile ----
-
-    # Get list of files
-    m_files <- list.files(
-      pcndir,
-      pattern = "Master_2021[0-9]{10}.xlsx"
+    ## Special national accounts --------
+    cl <- load_raw_aux(
+      measure = measure,
+      owner  = owner,
+      repo   = repo,
+      branch = branch,
+      tag    = tag
     )
 
-    # Find latest Masterfile
-    pcn_master_path <- m_files %>%
-      gsub("Master_|.xlsx", "", .) %>%
-      as.POSIXlt(format = "%Y%m%d%H%M%S") %>%
-      max(na.rm = TRUE) %>%
-      as.character() %>%
-      gsub("-|:| ", "", .) %>%
-      sprintf("%s/Master_%s.xlsx", pcndir, .)
-
-    # Read CountryList sheet from latest Masterfile
-    df <- readxl::read_xlsx(pcn_master_path, sheet = "CountryList")
-    df <- data.table::setDT(df)
-    df <- df[, c("CountryCode", "WBRegionCode")]
-    names(df) <- c("country_code", "pcn_region_code")
-
-
-    # ---- Read country data from WDI ----
-
-    country_list <- wbstats::wb_countries()
-    data.table::setDT(country_list)
-    country_list <- country_list[region != "Aggregates"]
-
-    # Recode names
-    setnames(country_list,
-      old = c("iso3c", "region_iso3c", "admin_region_iso3c"),
-      new = c("country_code", "region_code", "admin_region_code")
+    # Save
+    msrdir <- fs::path(maindir, "_aux", branch, measure) # measure dir
+    saved <- pip_sign_save(
+      x       = cl,
+      measure = measure,
+      msrdir  = msrdir,
+      force   = force
     )
 
-    # Merge w/ PCN data
-    country_list <- data.table::merge.data.table(
-      country_list, df,
-      all.x = TRUE, by = "country_code"
-    )
+    return(invisible(saved))
 
-    # Keep relevant variables
-    country_list <-
-      country_list[
-        ,
-        .(
-          country_code, country, region, region_code,
-          admin_region_code, pcn_region_code,
-          income_level, lending_type
-        )
-      ]
-
-    # ---- Save to file ----
-
-    pip_sign_save(
-      x = country_list,
-      measure = "country_list",
-      msrdir = msrdir,
-      force = force
-    )
-  } else if (action == "load") {
-    df <- load_aux(
-      maindir = maindir,
-      measure = measure
-    )
-    return(df)
   } else {
-    msg <- paste("action `", action, "` is not a valid action.")
-    rlang::abort(c(
-      msg,
-      i = "make sure you selected `update` or `load`"
-    ),
-    class = "pipaux_error"
-    )
+
+    df <- load_aux(maindir = maindir,
+                   measure = measure,
+                   branch  = branch)
+    return(df)
   }
 }
