5ebb24e17d390bb80a9cb47bdb8e820da7540ad8
R.Andres Castaneda <acastanedaa@worldbank.org>
2022-07-20 15:54:12 -0400

    update metadata documentation and update R script

---
+++ R/pip_metadata_update.R
@@ -0,0 +1,124 @@
+#' Update metadata file
+#'
+#' @inheritParams pip_pfw_update
+#'
+#' @return logical. TRUE if saved correctly. FALSE if error happened
+#' @export
+pip_metadata_update <- function(maindir = gls$PIP_DATA_DIR,
+                                force = FALSE,
+                                owner   = getOption("pipaux.ghowner"),
+                                branch  = c("DEV", "PROD", "main"),
+                                tag     = match.arg(branch)) {
+
+  measure <- "metadata"
+  branch <- match.arg(branch)
+
+  #   ____________________________________________________________________________
+  #   on.exit                                                                 ####
+  on.exit({
+
+  })
+
+  #   ____________________________________________________________________________
+  #   Defenses                                                                ####
+  stopifnot( exprs = {
+
+  }
+  )
+
+  #   ____________________________________________________________________________
+  #   Early returns                                                           ####
+  if (FALSE) {
+    return()
+  }
+
+  #   ____________________________________________________________________________
+  #   Computations                                                            ####
+
+  df <- load_raw_aux(measure = measure,
+                     owner = owner,
+                     branch = branch,
+                     tag = tag)
+
+  # Load pfw
+  pfw <- load_aux(measure = "pfw",
+                  maindir = maindir,
+                  branch = branch)
+
+
+
+  # Create distribution type column (data type)
+
+  pfw[,
+      domain_check := (gdp_domain == 2 | pce_domain == 2 |
+                         pop_domain == 2 | cpi_domain == 2 |
+                         ppp_domain == 2)]
+
+  # order  matters here
+  pfw[,
+      distribution_type  := fcase(
+        use_imputed   == 1, "micro, imputed",
+        use_microdata == 1, "micro",
+        use_groupdata == 1 & domain_check, "aggregated",
+        use_groupdata == 1, "group",
+        default = NA_character_
+      )
+  ]
+
+  # Merge datasets (inner join)
+  df <-
+    merge(df,
+          pfw[, c("country_code", "surveyid_year", "survey_acronym",
+                  "welfare_type", "reporting_year", "distribution_type",
+                  "surv_producer","survey_coverage", "surv_title",
+                  "link", "survey_year")],
+          by = "link", all.y = TRUE
+    )
+
+  # Recode colnames
+  df <- setnames(df,
+                 old = c("title", "surv_producer"),
+                 new = c("survey_title", "survey_conductor"))
+  df[,
+     survey_title := fifelse(is.na(survey_title), surv_title, survey_title)
+  ]
+
+  # Select columns
+  df <- df[,
+           c(
+             "country_code", "reporting_year",
+             "surveyid_year", "survey_year", "survey_acronym",
+             "survey_conductor", "survey_coverage",
+             "welfare_type", "distribution_type",
+             "survey_title", "year_start", "year_end",
+             "authoring_entity_name", "abstract",
+             "collection_dates_cycle", "collection_dates_start",
+             "collection_dates_end",
+             "sampling_procedure", "collection_mode",
+             "coll_situation", "weight", "cleaning_operations"
+           )
+  ]
+
+  # Create nested table
+
+  df <- tidyfast::dt_nest(df, country_code, reporting_year, survey_year,
+                          survey_title, survey_conductor, survey_coverage,
+                          welfare_type, distribution_type, .key = "metadata")
+
+
+##  ............................................................................
+##  Save                                                                    ####
+
+  msrdir <- fs::path(maindir, "_aux", branch, measure) # measure dir
+  saved <- pip_sign_save(
+    x       = df,
+    measure = measure,
+    msrdir  = msrdir,
+    force   = force
+  )
+
+  #   ____________________________________________________________________________
+  #   Return                                                                  ####
+  return(invisible(saved))
+
+}
