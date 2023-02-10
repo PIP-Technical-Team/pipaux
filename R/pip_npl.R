b439b53d008dc9cc3bca060ebf02320811e4df8e
Andres <acastastanedaa@worldbank.org>
2023-02-10 14:46:27 -0500

    add national poverty

---
+++ R/pip_npl.R
@@ -0,0 +1,67 @@
+#' National Poverty headcount
+#'
+#' Update series of national poverty lines
+#'
+#' @inheritParams pip_cpi
+#' @inheritParams pipfun::load_from_gh
+#' @export
+pip_npl <- function(action  = c("update", "load"),
+                   force   = FALSE,
+                   owner   = getOption("pipfun.ghowner"),
+                   maindir = gls$PIP_DATA_DIR,
+                   branch  = c("DEV", "PROD", "main"),
+                   tag     = match.arg(branch)) {
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ## setup --------
+
+  measure <- "npl"
+  branch <- match.arg(branch)
+  action <- match.arg(action)
+
+  if (action == "update") {
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ## update --------
+
+    npl <- pipfun::load_from_gh(measure = measure,
+                                owner  = owner,
+                                branch = branch,
+                                tag    = tag,
+                                ext    = "dta") |>
+      setDT()
+
+    setnames(x = npl,
+             old = c("countrycode",  "year", "vsi_pov_nahc_nc"),
+             new = c("country_code", "reporting_year", "nat_headcount"))
+
+    npl[, c("region", "vsi_pov_nahc") := NULL]
+    npl[, nat_headcount := nat_headcount / 100]
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ## save --------
+
+    if (branch == "main") {
+      branch <- ""
+    }
+    msrdir <- fs::path(maindir, "_aux", branch, measure) # measure dir
+
+    saved <- pipfun::pip_sign_save(
+      x       = npl,
+      measure = measure,
+      msrdir  = msrdir,
+      force   = force
+    )
+
+
+  } else {
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ## load --------
+
+    load_aux(
+      maindir = maindir,
+      measure = measure,
+      branch  = branch
+    )
+
+  }
+}
