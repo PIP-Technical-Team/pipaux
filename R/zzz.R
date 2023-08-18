2e5b09a66a3e722ed41df5ea3217f047f50b6a1d
Ronak Shah <shahronak47@yahoo.in>
2023-08-18 11:09:39 +0800

    include auto_aux_update fns

--- R/zzz.R
+++ R/zzz.R
@@ -13,6 +13,24 @@ pipuax_default_options <- list(
 
 )
 
+
+dependencies <- list(ppp = "country_list",
+                     pfw = character(),
+                     gdp = c("weo", "maddison", "wdi", "country_list"),
+                     wdi = character(),
+                     weo = c("pop"),
+                     pop = c("country_list", "pfw"),
+                     countries = c("pfw", "country_list"),
+                     metadata = "pfw",
+                     gdm = c("country_list", "pfw"),
+                     regions = c("country_list"),
+                     maddison = character(),
+                     country_list = character(),
+                     pce = c("wdi", "country_list"),
+                     cpi = "country_list",
+                     missing_data = c("country_list", "pce", "gdp", "pop", "pfw")
+)
+
 .onLoad <- function(libname, pkgname) {
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
