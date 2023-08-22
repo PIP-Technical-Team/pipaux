3787daa7d37ad32679a4c60aec4ef720f6a87ab9
Ronak Shah <shahronak47@yahoo.in>
2023-08-22 18:22:26 +0800

    comment zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,26 +1,26 @@
 
-gls <- pipfun::pip_create_globals()
-pipuax_default_options <- list(
-  pipaux.cpivar  = "cpi2011",
-  pipaux.pppvar  = "icp2011",
-  pipaux.pppyear = 2011,
-  pipaux.popsrc  = "emi",
-  pipaux.madsrc  = "https://www.rug.nl/ggdc/historicaldevelopment/maddison/data/mpd2020.dta",
-  pipaux.maindir = gls$PIP_DATA_DIR,
-  pipfun.ghowner = "PIP-Technical-Team",
-  joyn.verbose   = FALSE,
-  pipfun.verbose = TRUE
+#gls <- pipfun::pip_create_globals()
+#pipuax_default_options <- list(
+#  pipaux.cpivar  = "cpi2011",
+#  pipaux.pppvar  = "icp2011",
+#  pipaux.pppyear = 2011,
+#  pipaux.popsrc  = "emi",
+#  pipaux.madsrc  = "https://www.rug.nl/ggdc/historicaldevelopment/maddison/data/mpd2020.dta",
+#  pipaux.maindir = gls$PIP_DATA_DIR,
+#  pipfun.ghowner = "PIP-Technical-Team",
+#  joyn.verbose   = FALSE,
+#  pipfun.verbose = TRUE
 
-)
+#)
 
-.onLoad <- function(libname, pkgname) {
+#.onLoad <- function(libname, pkgname) {
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   ## Options --------
 
-  op    <- options()
-  toset <- !(names(pipuax_default_options) %in% names(op))
-  if (any(toset)) options(pipuax_default_options[toset])
+  #op    <- options()
+  #toset <- !(names(pipuax_default_options) %in% names(op))
+  #if (any(toset)) options(pipuax_default_options[toset])
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   ## defined values --------
@@ -28,6 +28,6 @@ pipuax_default_options <- list(
   # pipload::add_gls_to_env()
 
 
-  invisible()
-}
+  #invisible()
+#}
 
