f5f5c947ed6a47bc4990ddfe39e00490b7b78f63
Andres <acastastanedaa@worldbank.org>
2023-08-30 19:59:16 -0400

    uncommenting zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,33 +1,34 @@
-#
+
 # gls <- pipfun::pip_create_globals()
-# pipuax_default_options <- list(
-#   pipaux.cpivar  = "cpi2011",
-#   pipaux.pppvar  = "icp2011",
-#   pipaux.pppyear = 2011,
-#   pipaux.popsrc  = "emi",
-#   pipaux.madsrc  = "https://www.rug.nl/ggdc/historicaldevelopment/maddison/data/mpd2020.dta",
-#   pipaux.maindir = gls$PIP_DATA_DIR,
-#   pipfun.ghowner = "PIP-Technical-Team",
-#   joyn.verbose   = FALSE,
-#   pipfun.verbose = TRUE
-#
-# )
-#
-# .onLoad <- function(libname, pkgname) {
-#
-#   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#   ## Options --------
-#
-#   op    <- options()
-#   toset <- !(names(pipuax_default_options) %in% names(op))
-#   if (any(toset)) options(pipuax_default_options[toset])
-#
-#   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#   ## defined values --------
-#
-#   # pipload::add_gls_to_env()
-#
-#
-#   invisible()
-# }
-#
+pipuax_default_options <- list(
+  pipaux.cpivar  = "cpi2017",
+  pipaux.pppvar  = "icp2017",
+  pipaux.pppyear = 2017,
+  pipaux.popsrc  = "emi",
+  pipaux.madsrc  = "https://www.rug.nl/ggdc/historicaldevelopment/maddison/data/mpd2020.dta",
+  # pipaux.maindir = gls$PIP_DATA_DIR,
+  pipaux.maindir = pipfun::pip_create_globals()$PIP_DATA_DIR,
+  pipfun.ghowner = "PIP-Technical-Team",
+  joyn.verbose   = FALSE,
+  pipfun.verbose = TRUE
+
+)
+
+.onLoad <- function(libname, pkgname) {
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ## Options --------
+
+  op    <- options()
+  toset <- !(names(pipuax_default_options) %in% names(op))
+  if (any(toset)) options(pipuax_default_options[toset])
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ## defined values --------
+
+  # pipload::add_gls_to_env()
+
+
+  invisible()
+}
+
