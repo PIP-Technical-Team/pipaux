4b4b15703d4bfe697832a9fdecde28c32a5b4a3d
RossanaTat <rossana.tatulli@studbocconi.it>
2024-12-13 10:47:00 -0500

    documentation

--- R/utils.R
+++ R/utils.R
@@ -439,13 +439,11 @@ save_aux_to_gh <- function(df,
 
 #' Call auxiliary function for a specified measure
 #'
-#' @param measure character: Name of the auxiliary data measure (e.g., "ppp").
-#' @param package_name character: Name of the package containing the auxiliary function (default is "pipaux").
-#' @param ... additional arguments to pass to the auxiliary function.
+#' @param measure character: Name of the auxiliary data measure (e.g., "ppp")
+#' @param package_name character: Name of the package containing the auxiliary function (default is "pipaux")
+#' @param ... additional arguments to pass to the auxiliary function
 #'
 #' @return The result of the auxiliary function call.
-#' @examples
-#' \donotrun{call_aux_function("ppp")}
 call_aux_function <- function(measure,
                               package_name = "pipaux",
                               ...) {
