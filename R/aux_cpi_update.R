21437adecd3fe14c371e21a62ac27d316dcc4c7e
RossanaTat <rossana.tatulli@studbocconi.it>
2025-01-15 17:50:50 -0500

    more fixes:change name, rm arg, document, add comments

--- R/aux_cpi_update.R
+++ R/aux_cpi_update.R
@@ -38,7 +38,7 @@ aux_cpi_update <- function(maindir = gls$PIP_DATA_DIR,
                        maindir = maindir,
                        branch = branch)
 
-  # changae cpi_year and cpi_data_level to year and reporting_level
+  # change cpi_year and cpi_data_level to year and reporting_level
   cpi <- cpi |> setnames(c("cpi_year", "cpi_data_level"),
                          c("year", "reporting_level"),
                          skip_absent=TRUE)
--- R/utils.R
+++ R/utils.R
@@ -440,22 +440,26 @@ save_aux_to_gh <- function(df,
 #' Call auxiliary function for a specified measure
 #'
 #' @param measure character: Name of the auxiliary data measure (e.g., "ppp")
-#' @param package_name character: Name of the package containing the auxiliary function (default is "pipaux")
 #' @param ... additional arguments to pass to the auxiliary function
 #'
 #' @return The result of the auxiliary function call.
-aux <- function(measure,
-                              ...) {
+aux_f <- function(measure,
+                ...) {
 
   function_name <- paste0("aux_",
                           measure)
+
+  # This checks if the function exists in the {pipaux} package namespace
+  # -- The namespace corresponds to the version currently loaded in the R session
+  # -- (development version loaded via devtools::load_all() or the installed version)
 
   if (!exists(function_name,
               envir = asNamespace("pipaux"))) {
     cli::cli_abort(paste0("Function '", function_name, "' does not exist in the '", "pipaux package."))
   }
 
-  # Get the function
+  # Retrieve the function from the {pipaux} namespace
+  # -- The namespace refers to the version currently loaded in the R session
   func <- get(function_name,
               envir = asNamespace("pipaux"))
 
