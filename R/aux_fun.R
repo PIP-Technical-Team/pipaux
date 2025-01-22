2a27ad60cc45e1d068befb129d9a111ccbd436eb
RossanaTat <rossana.tatulli@studbocconi.it>
2025-01-22 17:53:15 -0500

    draft of check deps function

--- R/aux_fun.R
+++ R/aux_fun.R
@@ -27,3 +27,49 @@ aux_fun <- function(measure,
   # Call the function with additional arguments
   func(...)
 }
+
+# Check dependencies v0 test ####
+
+
+check_deps <- function(measure,
+                       ...) {
+
+  get_deps <- get_dependencies(measure)
+
+}
+
+# Get dependencies ####
+get_dependencies <- function(measure) {
+
+  # Locate config file with dependencies
+  yml_path <- system.file("extdata",
+                          "config.yml",
+                          package = "pipaux")
+
+  if (yml_path == "") {
+    stop("YAML file not found")
+  }
+
+  # Read the YAML file as a list
+  yml_data <- yaml::read_yaml(yml_path)
+
+  # Check if the measure exists in the YAML data
+  if (!measure %in% names(yml_data$default)) {
+    cli::cli_abort("measure {measure} not found")
+  }
+
+  # Extract dependencies for the given measure
+  deps <- yml_data$default[[measure]]
+
+  # If no dependencies
+  if (is.null(deps) || deps == "") {
+    return(character(0)) # Return empty vector for no dependencies
+  }
+
+  # Convert dependencies to a vector
+  deps_vector <- unlist(strsplit(deps,
+                                 ",\\s*"))
+
+  return(deps_vector)
+}
+
