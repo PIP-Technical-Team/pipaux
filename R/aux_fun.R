2121c5f761f17bbe9df622ef186a3984ebc78590
RossanaTat <rossana.tatulli@studbocconi.it>
2025-02-24 10:43:56 -0500

    another version of aux fun with for loop and trycatch

--- R/aux_fun.R
+++ R/aux_fun.R
@@ -4,7 +4,7 @@
 #' @param ... additional arguments to pass to the auxiliary function
 #'
 #' @return The result of the auxiliary function call
-aux_fun <- function(measure,
+aux_fun_v0 <- function(measure,
                     action = c("update", "load"),
                     repo = paste0("aux_", measure),
                     branch = paste0(release, "_", identity),
@@ -49,7 +49,7 @@ aux_fun <- function(measure,
   if (length(dependencies) >= 1) {
 
     lapply(dependencies, function(dep) {
-      aux_fun(measure  = measure,
+      aux_fun_v0(measure  = measure,
               action   = action,
               repo     = repo,
               branch   = release_branch,
@@ -111,16 +111,18 @@ aux_fun <- function(measure,
 }
 
 # Refactoring aux fun attempt one
-aux_fun_new <- function(measure,
-                    action    = c("update", "load"),
-                    repo      = paste0("aux_", measure),
-                    branch    = paste0(release, "_", identity),
-                    owner     = "RossanaTat",
-                    release,
-                    identity,
-                    maindir   = getOption("pipaux.working_dir"),
-                    processed = new.env(parent = emptyenv()),
-                    ...) {
+aux_fun_v1 <- function(measure,
+                        action    = c("update", "load"),
+                        repo      = paste0("aux_", measure),
+                        #branch    = paste0(release, "_", identity),
+                        owner     = "RossanaTat",
+                        release   = working_release$release,
+                        identity  = working_release$identity,
+                        maindir   = getOption("pipaux.working_dir"),
+                        processed = new.env(parent = emptyenv()),
+                        force     = FALSE,
+                        tag       = match.arg(branch),
+                        ...) {
 
   action         <- match.arg(action)
   release_branch <- paste0(release, "_", identity)
@@ -169,27 +171,29 @@ aux_fun_new <- function(measure,
   # _______________________________________________________ #####
 
   invisible(lapply(dependencies, function(dep) {
-    aux_fun_new(measure   = dep,
+    aux_fun_v1(measure   = dep,
                 action    = action,
-                repo      = paste0("aux_", dep),
+                repo      = repo,
                 branch    = release_branch,
                 owner     = owner,
                 release   = release,
                 identity  = identity,
                 maindir   = maindir,
                 processed = processed,
+                force     = force,
+                tag       = tag,
                 ...)
   }))
 
   # Check update status for the current measure
   # _______________________________________________________ #####
-  check_status_result <- check_status(measure = measure,
-                                      repo    = repo,
-                                      owner   = owner,
-                                      maindir = maindir)
+  check_status <- check_status(measure = measure,
+                               repo    = repo,
+                               owner   = owner,
+                               maindir = maindir)
 
-  update_gh <- check_status_result$update_gh
-  update_y  <- check_status_result$update_y
+  update_gh <- check_status$update_gh
+  update_y  <- check_status$update_y
 
   if (!update_gh && !update_y) {
     cli::cli_alert_info("No action required. GitHub and Y drive already up to date")
@@ -221,12 +225,131 @@ aux_fun_new <- function(measure,
       cli::cli_abort(paste0("Function '", function_name, "' does not exist in the 'pipaux' package."))
     }
 
-    func <- get(function_name, envir = asNamespace("pipaux"))
+    func <- get(function_name,
+                envir = asNamespace("pipaux"))
 
     func(action  = action,
          maindir = maindir,
          owner   = owner,
-         branch  = release_branch)
+         branch  = release_branch,
+         force   = force,
+         tag     = tag)
+  }
+
+  invisible(NULL)
+}
+
+# aux function new version
+aux_fun_new <- function(measure,
+                        action    = c("update", "load"),
+                        repo      = paste0("aux_", measure),
+                        owner     = "RossanaTat",
+                        release   = working_release$release,
+                        identity  = working_release$identity,
+                        maindir   = getOption("pipaux.working_dir"),
+                        processed = new.env(parent = emptyenv()),
+                        force     = FALSE,
+                        tag       = match.arg(branch),
+                        ...) {
+
+  action         <- match.arg(action)
+  release_branch <- paste0(release, "_", identity)
+
+  # Set repo to "Class" if measure is "income_groups" or "country_list"
+  repo <- if (measure %in% c("income_groups", "country_list")) "Class" else repo
+
+  # If measure has already been processed, return early
+  if (rlang::env_has(processed, measure)) {
+    return(invisible(NULL))
+  }
+
+  # Mark this measure as processed
+  rlang::env_poke(processed, measure, TRUE)
+
+  # Debug message on processing the current measure
+  cli::cli_alert_info("Processing measure: {measure}")
+
+  # Read all dependencies
+  dependencies_all <- read_dependencies(
+    gh_user = "https://raw.githubusercontent.com",
+    owner   = "PIP-Technical-Team"
+  )
+
+  # Get dependencies for this measure; if none, default to an empty vector
+  dependencies <- dependencies_all[[measure]]
+  if (is.null(dependencies)) {
+    dependencies <- character(0)
+  }
+
+  # Recursively process dependencies using a for loop with tryCatch
+  for (dep in dependencies) {
+    tryCatch(
+      {
+        aux_fun_new(
+          measure   = dep,
+          action    = action,
+          repo      = repo,
+          branch    = release_branch,
+          owner     = owner,
+          release   = release,
+          identity  = identity,
+          maindir   = maindir,
+          processed = processed,
+          force     = force,
+          tag       = tag,
+          ...
+        )
+      },
+      error = function(e) {
+        cli::cli_alert_danger("Error processing {dep}: {conditionMessage(e)}")
+      }
+    )
+  }
+
+  # Check update status for the current measure
+  check_status <- check_status(measure = measure,
+                               repo    = repo,
+                               owner   = owner,
+                               maindir = maindir)
+  update_gh <- check_status$update_gh
+  update_y  <- check_status$update_y
+
+  if (!update_gh && !update_y) {
+    cli::cli_alert_info("No action required. GitHub and Y drive already up to date")
+    return(invisible(NULL))
+  }
+
+  # Update: GH first and then Y
+  if (update_y) {
+
+    # Update GitHub if necessary
+    if (update_gh) {
+      pipfun::sync_release_branch(
+        owner      = owner,
+        repo       = repo,
+        ref_branch = "DEV",
+        release    = release,
+        identity   = identity
+      )
+    }
+
+    # Retrieve and execute the function from the pipaux namespace
+    function_name <- paste0("aux_", measure)
+
+    if (!exists(function_name, envir = asNamespace("pipaux"))) {
+      cli::cli_abort(paste0("Function '", function_name, "' does not exist in the 'pipaux' package."))
+    }
+
+    func <- get(function_name, envir = asNamespace("pipaux"))
+
+    func(
+      action  = action,
+      maindir = maindir,
+      owner   = owner,
+      branch  = release_branch,
+      force   = force,
+      tag     = tag
+    )
   }
 
   invisible(NULL)
@@ -234,7 +357,9 @@ aux_fun_new <- function(measure,
 
 
 
+
 # MEMO: ADD DOCUMENTATION
+## Check status ####
 check_status <- function(measure,
                          repo       = paste0("aux_", measure),
                          owner      = getOption("pipfun.ghowner"),
