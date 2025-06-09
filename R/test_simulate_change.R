3741a8fc2334d5f25bcaae83b39c514c3a801630
RossanaTat <rossana.tatulli@studbocconi.it>
2025-06-09 12:35:50 -0400

    changes to sim old release

--- R/test_simulate_change.R
+++ R/test_simulate_change.R
@@ -1,22 +1,28 @@
 #' Simulate an "old" version by modifying a measure's data file
 #'
-#' @param base_dir Root directory containing the version folders
-#' @param version The name of the old release folder to simulate changes in
-#' @param version The name of the current release folder
-#' @param measure The name of the measure folder (e.g. "gdp")
+#' @param base_dir Base directory containing the version folders
+#' @param root_dir Root directory of PIP data. Defaults tpo `Sys.getenv("PIP_ROOT_DIR")`
+#' @param old_release The name of the old release folder to simulate changes in
+#' @param measure Character. The name of the measure folder (e.g. "gdp")
 #' @param seed An optional seed for reproducibility
 #'
 #' @return Invisibly returns the modified data.table
 #' @keywords internal
-simulate_old_release <- function(base_dir = getOption("pipaux.working_dir"),
-                                 root_dir = Sys.getenv("PIP_ROOT_DIR"),
-                                 version = "20250101_TEST",
-                                 current_version = "20250203_TEST",
+simulate_old_release <- function(base_dir        = getOption("pipaux.working_dir"),
+                                 root_dir        = Sys.getenv("PIP_ROOT_DIR"),
+                                 old_release      = "20250101_TEST",
                                  measure,
-                                 seed = 123) {
+                                 seed            = 123) {
+
+  pipfun::get_wrk_release(verbose = FALSE)
+  release <- wrk_release$release
+  identity <- wrk_release$identity
+
+  current_version <- paste0(release, "_", identity)
+
   stopifnot(dir.exists(base_dir))
 
-  version_path <- file.path(root_dir, base_dir, version)
+  version_path <- file.path(root_dir, base_dir, old_release)
   measure_path <- file.path(version_path, measure)
   file_path <- file.path(measure_path, paste0(measure, ".qs"))
 
@@ -71,7 +77,7 @@ simulate_old_release <- function(base_dir = getOption("pipaux.working_dir"),
   # SAve modified data
   pipfun::pip_sign_save(x       = dt,
                         measure = measure,
-                        msrdir  = fs::path(maindir, "aux_data", version, measure))
+                        msrdir  = fs::path(maindir, "aux_data", old_release, measure))
 
   message("Modified and saved simulated old version at: ", file_path)
 
