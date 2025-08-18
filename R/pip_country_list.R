03f0c568d648f3549bb21e7cb5dc1ac239aed4f9
R.Andres Castaneda <acastastanedaa@worldbank.org>
2025-08-18 19:16:12 -0400

    update saving of country_list

--- R/pip_country_list.R
+++ R/pip_country_list.R
@@ -40,27 +40,18 @@ pip_country_list <- function(action = c("update", "load"),
     )
 
     if (saved) {
-      cl_sha <- digest::sha1(cl)
-      out <- gh::gh(
-        "GET /repos/{owner}/{repo}/contents/{path}",
-        owner     = "PIP-Technical-Team",
-        repo      = "aux_country_list",
-        path      = "sha_country_list.txt",
-        .params   = list(ref = "DEV")
-      )
 
-      res <- gh::gh(
-        "PUT /repos/{owner}/{repo}/contents/{path}",
-        owner   = "PIP-Technical-Team",
-        repo    = "aux_country_list",
-        path    = "sha_country_list.txt",
-        .params = list(
-          branch  = branch,
-          message = paste0("update on ", prettyNum(Sys.time())),
-          sha     = out$sha,
-          content = base64enc::base64encode(charToRaw(cl_sha))
-        )
-      )
+      pipfun::save_to_gh(df = cl,
+                         measure = measure,
+                         branch = branch,
+                         filename = measure)
+
+      cl_sha <- digest::sha1(cl)
+      pipfun::save_to_gh(df = cl_sha,
+                         measure = measure,
+                         branch = branch,
+                         filename = "sha_country_list",
+                         ext      = "txt")
 
     }
 
