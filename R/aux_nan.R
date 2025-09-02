113a2214c5f24566a8f05e13b0b778ce05e23c1c
RossanaTat <rossana.tatulli@studbocconi.it>
2025-09-02 11:58:22 -0400

    smae for aux nan

--- R/aux_nan.R
+++ R/aux_nan.R
@@ -9,14 +9,13 @@
 #' @export
 aux_nan <- function(action          = c("update", "load"),
                     force           = FALSE,
-                    maindir         = getOption("pipaux.working_dir"),
                     owner           = getOption("pipfun.ghowner"),
                     tag             = NULL) {
 
   measure    <- "nan"
   action <- match.arg(action)
 
-  pipfun::get_wrk_release(verbose = FALSE)
+  wrk_release <- get_from_auxenv(key = "wrk_release")
 
   release        <- wrk_release$release
   identity       <- wrk_release$identity
@@ -35,10 +34,10 @@ aux_nan <- function(action          = c("update", "load"),
       branch = branch,
       filename = "nan.csv"
     )
+
     if (branch == "main") {
       branch <- ""
     }
-    msrdir <- fs::path(maindir, "aux_data", branch, measure) # measure dir
 
     # ----- function raw sha ----------------------
 
@@ -51,19 +50,15 @@ aux_nan <- function(action          = c("update", "load"),
             "raw_sha_fun",
             raw_sha_fun)
 
-    saved <- pipfun::pip_sign_save(
-      x       = nan,
-      measure = measure,
-      msrdir  = msrdir,
-      force   = force
+    saved <- pip_aux_save(
+      x        = nan,
+      pin_name = measure,
+      force    = force
     )
 
   } else {
-    dt <- load_aux(
-      maindir = maindir,
-      measure = measure,
-      branch  = branch
-    )
+    dt <- pipload::load_aux_data(measure = measure)
+
     return(dt)
   }
-} # end of pip_gdp
+}
