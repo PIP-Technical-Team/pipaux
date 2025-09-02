b6e68b07866179bc1f9a1883f03f0aac1551dbe9
RossanaTat <rossana.tatulli@studbocconi.it>
2025-09-02 15:09:53 -0400

    aux weo updated

--- R/aux_weo.R
+++ R/aux_weo.R
@@ -16,14 +16,13 @@
 aux_weo <- function(action  = c("update", "load"),
                     force   = FALSE,
                     owner   = getOption("pipfun.ghowner"),
-                    maindir = getOption("pipaux.working_dir"),
                     tag     = NULL,
                     detail  = getOption("pipaux.detail.raw")) {
 
   measure <- "weo"
   action <- match.arg(action)
 
-  pipfun::get_wrk_release(verbose = FALSE)
+  wrk_release <- get_from_auxenv(key = "wrk_release")
 
   release        <- wrk_release$release
   identity       <- wrk_release$identity
@@ -53,7 +52,6 @@ aux_weo <- function(action  = c("update", "load"),
     weo_validate_raw(weo = dt, detail = detail)
 
     dt <- aux_weo_clean(dt,
-                        maindir = maindir,
                         branch = branch)
 
     # Save dataset
@@ -79,23 +77,19 @@ aux_weo <- function(action  = c("update", "load"),
     if (branch == "main") {
       branch <- ""
     }
-  msrdir <- fs::path(maindir, "aux_data", branch, measure) # measure dir
 
-  cat('\nDir : ', msrdir)
-    saved <- pipfun::pip_sign_save(
-      x       = dt,
-      measure = measure,
-      msrdir  = msrdir,
-      force   = force
+    saved <- pip_aux_save(
+      x        = dt,
+      pin_name = measure,
+      force    = force
     )
+
     return(invisible(saved))
 
   } else {
-    dt <- load_aux(
-      maindir = maindir,
-      measure = measure,
-      branch  = branch
-    )
+
+    dt <- pipload::load_aux_data(measure = measure)
+
     return(dt)
   }
 }
@@ -109,21 +103,7 @@ aux_weo <- function(action  = c("update", "load"),
 #' @return data.table
 #' @export
 aux_weo_clean <- function(dt,
-                          maindir = getOption("pipaux.working_dir"),
                           branch  = NULL) {
-
-  pipfun::get_wrk_release(verbose = FALSE)
-
-  if (is.null(branch)) {
-    release        <- wrk_release$release
-    identity       <- wrk_release$identity
-    branch         <- paste0(release, "_", identity)
-
-
-  }
-
-
-
 
   #   _________________________________________
   #   Computations                        ####
@@ -203,9 +183,7 @@ aux_weo_clean <- function(dt,
   # ---- Merge with population ----
 
 
-  pop <- load_aux(measure = "pop",
-                  maindir = maindir,
-                  branch = branch)
+  pop <- pipload::load_aux_data(measure = "pop")
 
   setDT(pop)
   pop <- pop[reporting_level == "national", ] #pop_data_level = reporting_level
@@ -223,15 +201,6 @@ aux_weo_clean <- function(dt,
   dt[, weo_gdp := chain_val(ori_var = weo_gdp_ppp2017,
                             rep_var = weo_gdp_lcu),
      by = country_code]
-  #
-  # dt <- chain_values(
-  #   dt,
-  #   base_var        = "weo_gdp_ppp2017",
-  #   replacement_var = "weo_gdp_lcu",
-  #   new_name        = "weo_gdp",
-  #   by              = "country_code"
-  # )
-
 
   # --- Sign and save ----
 
