3a2fa64f2d67c2b9e063477cf1b736aad88e78f9
RossanaTat <rossana.tatulli@studbocconi.it>
2026-02-17 16:18:41 -0500

    fix log save - to test

--- R/update_aux_data.R
+++ R/update_aux_data.R
@@ -339,7 +339,8 @@ update_aux_measures <- function(measures,
   
   # Save the log if requested
   if (log_save && !is.null(log_name)) {
-    pipfun::log_save(name = log_name)
+    pipfun::log_save(name = log_name,
+    alias = get_from_auxenv("aux_meta_alias"))
   }
   
   invisible(NULL)
