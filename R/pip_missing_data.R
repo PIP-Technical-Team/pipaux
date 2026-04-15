4269ddcbd839180a9401af5c4d6d5a580baf7c80
R.Andres Castaneda <acastanedaa@worldbank.org>
2026-04-15 14:34:48 -0400

    re format

--- R/pip_missing_data.R
+++ R/pip_missing_data.R
@@ -6,74 +6,65 @@
 #' @return if `action = "update"` returns logical. If `action = "load"` returns
 #'   a data.table
 #' @export
-pip_missing_data <- function(action  = c("update", "load"),
-                             force   = FALSE,
-                             owner   = getOption("pipfun.ghowner"),
-                             maindir = gls$PIP_DATA_DIR,
-                             branch  = c("DEV", "PROD", "main"),
-                             tag     = match.arg(branch)
-                             ) {
-
+pip_missing_data <- function(
+  action = c("update", "load"),
+  force = FALSE,
+  owner = getOption("pipfun.ghowner"),
+  maindir = gls$PIP_DATA_DIR,
+  branch = c("DEV", "PROD", "main"),
+  tag = match.arg(branch)
+) {
   measure <- "missing_data"
   branch <- match.arg(branch)
   action <- match.arg(action)
 
-#   ________________________________________________________________
-#   Computations                                              ####
-
+  #   ________________________________________________________________
+  #   Computations                                              ####
 
   if (action == "update") {
-
     # Load data ------
 
     # pipload::pip_load_all_aux(aux = c("gdp", "pce", "pfw", "country_list"),
     #                           aux_names = c("gdp", "pce", "pfw", "cl"),
     #                           replace = TRUE)
 
-    gdp <- load_aux(maindir = maindir,
-                    branch  = branch,
-                    measure = "gdp")
+    gdp <- load_aux(maindir = maindir, branch = branch, measure = "gdp")
 
-    pce <- load_aux(maindir = maindir,
-                    branch  = branch,
-                    measure = "pce")
+    pce <- load_aux(maindir = maindir, branch = branch, measure = "pce")
 
-    pfw <- load_aux(maindir = maindir,
-                    branch  = branch,
-                    measure = "pfw")
+    pfw <- load_aux(maindir = maindir, branch = branch, measure = "pfw")
 
-    pop <- load_aux(maindir = maindir,
-                    branch  = branch,
-                    measure = "pop")
+    pop <- load_aux(maindir = maindir, branch = branch, measure = "pop")
 
-    cl <- load_aux(maindir = maindir,
-                   branch  = branch,
-                    measure = "country_list")
-
+    cl <- load_aux(maindir = maindir, branch = branch, measure = "country_list")
 
     ref_years <- gls$PIP_REF_YEARS
 
     # Prepare NAC data ------
 
     # Standardize ^_data_level ^_domain column names
-    setnames(pce, names(pce) ,sub("^pce[_]", "nac_", names(pce)))
-    setnames(gdp, names(gdp) ,sub("^gdp[_]", "nac_", names(gdp)))
+    setnames(pce, names(pce), sub("^pce[_]", "nac_", names(pce)))
+    setnames(gdp, names(gdp), sub("^gdp[_]", "nac_", names(gdp)))
 
-    nac <- joyn::joyn(gdp, pce,
-                       by = c(
-                         "country_code", "year", "nac_data_level",
-                         "nac_domain"),
-                       match_type = "1:1",
-                       reportvar = FALSE,
-                       verbose = FALSE,
-                      keep = "full")
+    nac <- joyn::joyn(
+      gdp,
+      pce,
+      by = c(
+        "country_code",
+        "year",
+        "nac_data_level",
+        "nac_domain"
+      ),
+      match_type = "1:1",
+      reportvar = FALSE,
+      verbose = FALSE,
+      keep = "full"
+    )
 
     nac <-
-      nac[year %in% ref_years
-      ][,
-        nac_domain := NULL]
-
-
+      nac[year %in% ref_years][,
+        nac_domain := NULL
+      ]
 
     # Prepare Grid of all countries and ref years ---------
 
@@ -88,46 +79,49 @@ pip_missing_data <- function(action  = c("update", "load"),
     # domvar <- grep("domain$", names(pfw), value = TRUE) # all domains
     domvar <- grep("(gdp|pce)_domain$", names(pfw), value = TRUE) # data_level vars
     idvars <- c("country_code", "year", "welfare_type")
-    keepv  <- c(idvars, domvar)
-    dom <- pfw[inpovcal == 1
-    ][, ..keepv]
+    keepv <- c(idvars, domvar)
+    dom <- pfw[inpovcal == 1][, ..keepv]
 
     # pfw[, mdom := max(.SD[, mget(domvar)]), by = .I]  # when 1.14.3 becomes available
     dom[,
-        #  Create row position (must be done in 1.14.2)
-        n := .I
+      #  Create row position (must be done in 1.14.2)
+      n := .I
     ][,
       # max reporting year per country/year
-      mdom := max(.SD[, mget(domvar)]), by = n
+      mdom := max(.SD[, mget(domvar)]),
+      by = n
     ]
 
     dom <- dom[,
-               .(country_code, year, welfare_type, mdom)]
+      .(country_code, year, welfare_type, mdom)
+    ]
 
     ## National data ------------
     domn <- dom[mdom == 1]
 
     domn <- domn[,
-                 nac_data_level := "national"]
+      nac_data_level := "national"
+    ]
 
     ## Urban/Rural data -----------
-    domu <-  dom[mdom == 2]
-    domr <-  dom[mdom == 2]
+    domu <- dom[mdom == 2]
+    domr <- dom[mdom == 2]
 
     domu <- domu[,
-                 nac_data_level := "urban"]
+      nac_data_level := "urban"
+    ]
 
     domr <- domr[,
-                 nac_data_level := "rural"]
+      nac_data_level := "rural"
+    ]
 
     ## append national and urban/rural sections ----
 
     dom2 <-
-      list(domn, domu, domr)  |>
+      list(domn, domu, domr) |>
       rbindlist()
 
     dom2 <- unique(dom2[, c("country_code", "year", "nac_data_level")])
-
 
     ## Survey availability -----------
     svav <- unique(pfw[, .(country_code, year)])
@@ -145,16 +139,17 @@ pip_missing_data <- function(action  = c("update", "load"),
     # as well Those that are in NAC but are not ingrid are those without survey,
     # which we took care above
 
-    ct_nonac <- joyn::joyn(ct_sv, nac[!is.na(gdp) | !is.na(pce)],
-                            by = c("country_code", "year"),
-                            match_type = "1:m",
-                            verbose = FALSE)
+    ct_nonac <- joyn::joyn(
+      ct_sv,
+      nac[!is.na(gdp) | !is.na(pce)],
+      by = c("country_code", "year"),
+      match_type = "1:m",
+      verbose = FALSE
+    )
     ct_nonac <-
-      ct_nonac[.joyn == "x"
-               ][,
-                 c("country_code", "year")
-                 ]
-
+      ct_nonac[.joyn == "x"][,
+        c("country_code", "year")
+      ]
 
     # Get Countries with missing data ---------
 
@@ -162,90 +157,82 @@ pip_missing_data <- function(action  = c("update", "load"),
       list(ct_nosv, ct_nonac) |>
       rbindlist(use.names = TRUE)
 
-
     setorder(ct_miss_data, country_code, year)
 
+    # Add region_code -------
+    ct_miss_data <- joyn::joyn(
+      ct_miss_data,
+      cl,
+      by = "country_code",
+      keep = "left",
+      match_type = "m:1",
+      reportvar = FALSE,
+      verbose = FALSE
+    )
 
-# Add region_code -------
-  ct_miss_data <- joyn::joyn(ct_miss_data, cl,
-                              by = "country_code",
-                              keep = "left",
-                              match_type = "m:1",
-                              reportvar = FALSE,
-                              verbose = FALSE)
-
-
-# Join  with pop data -----
+    # Join  with pop data -----
     pop <-
-      pop[pop_data_level == "national"
-      ][,
+      pop[pop_data_level == "national"][,
         c("pop_domain", "pop_data_level") := NULL
       ]
 
     setnames(pop, "pop", "reporting_pop")
     # Filter pop with missing data countries
     pop_md <-
-      pop[ct_miss_data,
-          on = c("country_code", "year")]
+      pop[ct_miss_data, on = c("country_code", "year")]
 
-
-# Add welfare type -------------
+    # Add welfare type -------------
 
     dt <- pipfun::load_from_gh(
-      measure  = "country_list",
-      owner    = "GPID-WB",
-      repo     = "Class",
-      branch   = "master",
+      measure = "country_list",
+      owner = "GPID-WB",
+      repo = "Class",
+      branch = "master",
       filename = "OutputData/CLASS",
-      ext      = "dta"
+      ext = "dta"
     ) |>
       setDT() |>
       setorder(code, year_data) |>
-      fselect(year = year_data,
-              country_code = code,
-              incgroup_code)
+      fselect(year = year_data, country_code = code, incgroup_code)
 
-    md <- joyn::joyn(pop_md, dt,
-                     by = c("country_code", "year"),
-                     match_type = "1:1",
-                     keep = "left",
-                     reportvar = FALSE)
-
-    md[, incgroup_code := na_focb(incgroup_code),
-       by = country_code
-       ][, incgroup_code := na_locf(incgroup_code),
-         by = country_code
-       ][,
-         welfare_type := fifelse(incgroup_code == "HIC",
-                                 "income", "consumption")
-         ][is.na(incgroup_code),
-           welfare_type := "consumption"
-           ][,
-           incgroup_code := NULL]
-
-
-
-
-#  .................................................................
-##  Save data                                                    ####
-    msrdir  <- fs::path(maindir, "_aux/", branch, measure)
-
-    saved <- pipfun::pip_sign_save(
-      x       = md,
-      measure = measure,
-      msrdir  = msrdir,
-      force   = force
+    md <- joyn::joyn(
+      pop_md,
+      dt,
+      by = c("country_code", "year"),
+      match_type = "1:1",
+      keep = "left",
+      reportvar = FALSE
     )
 
-    return(invisible(saved))
+    md[, incgroup_code := na_focb(incgroup_code), by = country_code][,
+      incgroup_code := na_locf(incgroup_code),
+      by = country_code
+    ][,
+      welfare_type := fifelse(incgroup_code == "HIC", "income", "consumption")
+    ][is.na(incgroup_code), welfare_type := "consumption"][,
+      incgroup_code := NULL
+    ]
 
+    #  .................................................................
+    ##  Save data                                                    ####
+    msrdir <- fs::path(maindir, "_aux/", branch, measure)
+
+    saved <- pipfun::pip_sign_save(
+      x = md,
+      measure = measure,
+      msrdir = msrdir,
+      force = force
+    )
+    # Push data (md) to GitHub as gdp.csv
+    pipfun::save_to_gh(md, measure = measure, branch = branch)
+
+    return(invisible(saved))
   } else {
     dl <- load_aux(
       maindir = maindir,
       measure = measure,
-      branch  = branch
+      branch = branch
     )
     return(dl)
   }
-
 } # end of function pip_missing_data
